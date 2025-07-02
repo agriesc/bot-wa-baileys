bd086f277ba5aa33658a274648ff2096d3a00c99
agriesc <rismawanagrie@gmail.com>
2025-07-02 14:58:52 +0700

    fix:credentials

--- bot/commands/laporan.js
+++ bot/commands/laporan.js
@@ -1,5 +1,13 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const creds = require("../../credentials.json");
+const creds = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+};
+
 const { parseDateFromCell, getDateRangeByType } = require("../utils/tanggal");
 const { parseNominal } = require("../utils/nominal");
 const { formatIDR } = require("../utils/formatIDR");
